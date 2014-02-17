1ed6477cf04aa9f360cdde4e2f130ab91dd25684
dyma96 <dyma96@bk.ru>
2014-02-17 16:39:33 +0400

    +7sortColumn 12.02 without sort

---
+++ 2term/02.12/7sortColumn/7sortColumn/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7sortColumn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size array:");
+            //string input = Console.ReadLine();
+            int sizeArray = Convert.ToInt32(Console.ReadLine());
+            int[,] mas = new int[sizeArray,sizeArray];
+            Random rand = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < sizeArray; i++)
+                for (int j = 0; j < sizeArray; j++)
+                    mas[i,j] = rand.Next(0, 100);
+
+        }
+    }
+}
