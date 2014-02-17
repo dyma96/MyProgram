3175f760781fe29a6d8ac511536eb74c98e5bc7b
dyma96 <dyma96@bk.ru>
2014-02-17 13:08:34 +0400

    +3sort 12.02

---
+++ 2term/02.12/3sort/3sort/Program.cs
@@ -0,0 +1,82 @@
+﻿/* Test is Ok -
+ *              2: 87, 22
+ *              5: 13, 73, 67, 60, 33
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3sort
+{
+    class Program
+    {
+        
+        static void swap(ref int swapOne, ref int swapTwo)
+        {
+	        int swapSwap = swapOne;
+	        swapOne = swapTwo;
+	        swapTwo = swapSwap;
+        }
+
+        static void qsort(int[] mas, int begin, int end)
+        {
+	        int general = 0;
+	        int i = 0;
+	        for (i = 0; i < end - begin && mas[begin + i] == mas[begin]; i++) ;
+	
+	        if (mas[begin] == mas[begin + i])
+		        return;
+	        else if (mas[begin] < mas[begin + i])
+		        general = mas[begin + i];
+	        else
+		        general = mas[begin];
+	
+	        int left = begin;
+	        int right = end;
+
+	        while (left < right)
+	        {
+		        while (mas[left] < general)
+			        left++;
+		        while (mas[right] >= general)
+			        right--;
+		        if (left >= right)
+			        break;
+		        swap(ref mas[left], ref mas[right]);
+
+		        left++;
+		        right--;
+	        }		
+	        qsort(mas, begin, right);
+	        qsort(mas, left, end);	
+        }
+
+
+        static void printMas(int[] mas, int size)
+        {
+            for (int i = 0; i < size; i++)
+                Console.Write(" {0} ", mas[i]);
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter size of array:");
+            string input = Console.ReadLine();
+            int sizeArray  = Convert.ToInt32(input); 
+            int[] mas = new int[sizeArray];
+            Random rand = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < sizeArray; i++)
+                mas[i] = rand.Next(0,100);
+
+            Console.WriteLine("Not sort mas:");
+            printMas(mas, sizeArray);
+            qsort(mas, 0, sizeArray-1);
+            Console.WriteLine("Sort mas:");
+            printMas(mas, sizeArray);
+            
+        }
+    }
+}
