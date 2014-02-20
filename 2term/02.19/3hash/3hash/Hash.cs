679288b6249ae7343f38513238fa25392db13417
dyma96 <dyma96@bk.ru>
2014-02-20 22:05:53 +0400

    +3hash 19.02.14

---
+++ 2term/02.19/3hash/3hash/Hash.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NList;
+
+using ElementType = System.Int32;
+
+namespace NHash
+{
+    class Hash
+    {
+        public Hash(int size)
+        {
+            bucket = new List[size];
+            for (int i = 0; i < size; i++)
+                bucket[i] = new List();
+        }
+        
+        static int HashFunction(ElementType newElement)
+        {
+            return (newElement * (newElement + 5 * newElement % 3) / 2);
+        }
+
+        public void AddToHashTable(ElementType newElement)
+        {
+            if (ExistHash(newElement))
+            {
+                Console.WriteLine("Element exist");
+                return;
+            }
+            bucket[HashFunction(newElement) % bucket.GetLength(0)].AddElementToHead(newElement);
+        }
+
+        public void DeleteElement(ElementType deleteElement)
+        {
+            if (!ExistHash(deleteElement))
+            {
+                Console.WriteLine("Element not exist");
+                return;
+            }
+            bucket[HashFunction(deleteElement) % bucket.GetLength(0)].DeleteElement(deleteElement);
+        }
+        
+        public bool ExistHash(ElementType element)
+        {
+            return bucket[HashFunction(element) % bucket.GetLength(0)].ExistList(element);
+        }
+
+        public void PrintHash()
+        {
+            for (int i = 0; i < bucket.GetLength(0); i++)
+            {
+                bucket[i].Print();
+            }
+        }
+        private List[] bucket;
+//        private const int size = 10;
+    }
+}
---
+++ 2term/02.19/3hash/3hash/Program.cs
@@ -0,0 +1,56 @@
+﻿/* Test is Ok -
+ *             1 - 1, 2, 3
+ *             3 - 1: exist
+ *             2 - 1
+ *             3 - 1: not exist
+ *             2 - 2, 3
+ *             2 - 1.
+ */
+using System;
+using NHash;
+
+namespace _3hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of hash table");
+            Hash hash = new Hash(Convert.ToInt32(Console.ReadLine()));       
+            int whatDo = -1;
+            while (whatDo != 0)
+            {
+                Console.WriteLine("0 - exit");
+                Console.WriteLine("1 - add element");
+                Console.WriteLine("2 - delete element");
+                Console.WriteLine("3 - exist element");
+                whatDo = Convert.ToInt32(Console.ReadLine());
+                if (whatDo == 1)
+                {
+                    int newNum = 0;
+                    Console.WriteLine("Enter number for add - ");
+                    newNum = Convert.ToInt32(Console.ReadLine());
+                    hash.AddToHashTable(newNum);
+                }
+                if (whatDo == 2)
+                {
+                    int deleteNum = 0;
+                    Console.WriteLine("Enter number for delete - ");
+                    deleteNum = Convert.ToInt32(Console.ReadLine());
+                    hash.DeleteElement(deleteNum);
+                }
+                if (whatDo == 3)
+                {
+                    int existNum = 0;
+                    Console.WriteLine("Enter number for exist - ");
+                    existNum = Convert.ToInt32(Console.ReadLine());
+                    if (hash.ExistHash(existNum))
+                        Console.WriteLine("{0} exist", existNum);
+                    else
+                        Console.WriteLine("{0} not exist", existNum);
+                }
+//                hash.PrintHash();
+            }
+        }
+    }
+}
