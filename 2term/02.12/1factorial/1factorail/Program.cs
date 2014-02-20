c2ac35438a1a12f8bc932bc0508e87a23b6e0f44
dyma96 <dyma96@bk.ru>
2014-02-20 18:15:13 +0400

    1, 2 program 12.02.14. Change in style

---
+++ 2term/02.12/1factorial/1factorail/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Test is Ok - 
+ *              3! = 6
+ *              5! = 120
+ */
+using System;
+
+namespace _1factorail
+{
+    class Program
+    {
+        // Подсчет факториала числа num.
+        static int Factorial(int num)
+        {
+            if (num == 1)
+                return 1;
+            return num * Factorial(num - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("Enter number for factorial:");
+            string input = Console.ReadLine();
+            num = Convert.ToInt32(input);
+            Console.WriteLine("{0}! = {1}", num, Factorial(num));
+        }
+    }
+}
---
+++ 2term/02.12/2fibonacci/2fibonachi/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Test is Ok -
+ *              1 = 1
+ *              3 = 2
+ *              10 = 55
+ */
+using System;
+
+namespace _2Fibonacci
+{
+    class Program
+    {
+        // Подсчет числа Фибоначчи с номером num.
+        static int Fibonacci(int num)
+        {
+            if (num <= 2)
+                return 1;
+            return Fibonacci(num - 1) + Fibonacci(num - 2);
+        }
+        
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter number of Fibonacci:");
+            string input = Console.ReadLine();
+            int num = Convert.ToInt32(input);
+            System.Console.WriteLine("{0} number Fibonacci = {1}", num, Fibonacci(num));
+        }
+    }
+}
