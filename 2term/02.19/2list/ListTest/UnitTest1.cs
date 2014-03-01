477b0dd8709279dfc119d162c7e5e0d034c5e37f
dyma96 <dyma96@bk.ru>
2014-03-01 19:59:03 +0400

    2list + test.

---
+++ 2term/02.19/2list/ListTest/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace List.Test
+{
+    using NList;
+
+    [TestClass]
+    public class ListTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            list = new List();
+        }
+
+        [TestMethod]
+        public void AddElementTest()
+        {
+            list.AddElement(2);
+            Assert.IsTrue(list.Exist(2));
+        }
+        
+        [TestMethod]
+        public void PositionElementTest()
+        {
+            list.AddElement(1);
+            Assert.IsTrue(list.PositionElement(1) == 0);
+        }
+
+        [TestMethod]
+        public void AddElementToHeadTest()
+        {
+            list.AddElement(2);
+            list.AddElementToHead(1);
+            Assert.IsTrue(list.PositionElement(1) == 0);
+        }
+
+        [TestMethod]
+        public void AddElementToPositionTest()
+        {
+            list.AddElement(2);
+            list.AddElement(4);
+            list.AddElementToPosition(3, 2);
+            Assert.IsTrue(list.PositionElement(3) == 2);
+        }
+
+        [TestMethod]
+        public void DeleteElementTest()
+        {
+            list.AddElement(2);
+            list.DeleteElement(2);
+            Assert.IsFalse(list.Exist(2));
+        }
+
+        private List list;
+    }
+}
