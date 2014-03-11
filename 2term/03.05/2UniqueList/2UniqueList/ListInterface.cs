bacfdf86537f52b489635ae2e796cd7e8af4d1ea
dyma96 <dyma96@bk.ru>
2014-03-11 16:20:35 +0400

    2UniqueList

---
+++ 2term/03.05/2UniqueList/2UniqueList/ListInterface.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2UniqueList
+{
+    public interface ListInterface
+    {
+        // add element to end with value value.
+        void AddElement(int value);
+
+        // add element after position pos.
+        //void AddElementToPosition(int value, int pos);
+
+        // add element to head with value value.
+        void AddElementToHead(int value);
+
+        // delete element with value deleteNum.
+        void DeleteElement(int deleteNum);
+
+        //return position element with vale value.
+        //int PositionElement(int value);
+        
+        // return true or false - exist element value in stack.
+        bool Exist(int value);
+
+        // print stack.
+        void Print();
+    }
+}
---
+++ 2term/03.05/2UniqueList/2UniqueList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2UniqueList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueList list = new UniqueList();
+            list.AddElement(2);
+            try
+            {
+                list.AddElement(2);
+            }
+            catch (ExceptionUniqueList except)
+            {
+                Console.WriteLine("Error: " + except.Message);
+            }
+        }
+    }
+}
