b40e63410001361c5db49caa02a349e9291f8e87
dyma96 <dyma96@bk.ru>
2014-03-16 13:54:43 +0400

    12.03 - 4CursorControl.

---
+++ 2term/03.12/4CursorControl/CursorControl/CursorControl.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4CursorControl
+{
+    public class CursorControl
+    {
+        public void OnLeft(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft - 1 >= 0)
+            {
+                Console.CursorLeft--;
+                if (Console.CursorLeft + 1 < 80)
+                {
+                    Console.Write("_");
+                    Console.CursorLeft--;
+                }
+            }
+        }
+
+        public void OnRight(object sender, EventArgs args)
+        {
+            if (Console.CursorLeft + 1 < 79)
+                Console.CursorLeft++;
+            if (Console.CursorLeft + 1 < 80)
+            {
+                Console.Write("_");
+                Console.CursorLeft--;
+            }
+        }
+
+        public void OnUp(object sender, EventArgs args)
+        {
+            if (Console.CursorTop - 1 > 0)
+            {
+                if (Console.CursorLeft + 1 < 80)
+                {
+                    Console.Write("|");
+                    Console.CursorLeft--;
+                }
+                Console.CursorTop--;
+            }
+        }
+        
+        public void OnDown(object sender, EventArgs args)
+        {
+            if (Console.CursorTop + 1 < 25)
+            {
+                Console.CursorTop++;
+                if (Console.CursorLeft + 1 < 80)
+                {
+                    Console.Write("|");
+                    Console.CursorLeft--;
+                }
+            }
+        }
+    }
+}
---
+++ 2term/03.12/4CursorControl/CursorControl/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _4CursorControl
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Escape to exit.");
+            var eventLoop = new EventLoop();
+            var cursor = new CursorControl();
+
+            eventLoop.LeftHandler += cursor.OnLeft;
+            eventLoop.RightHandler += cursor.OnRight;
+            eventLoop.UpHandler += cursor.OnUp;
+            eventLoop.DownHandler += cursor.OnDown;
+
+            eventLoop.Run();
+        }
+    }
+}
