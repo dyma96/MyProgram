3ad5c8331fc1f4329a67ece161d67a91c41026e5
dyma96 <dyma96@bk.ru>
2014-03-26 12:09:58 +0400

    class work - X0.

---
+++ 2term/kontr/X0/X0/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace X0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.MinimumSize = new System.Drawing.Size(284, 262);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(284, 262);
+            this.tableLayoutPanel1.TabIndex = 0;
+
+            InitializeButtons();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "X0";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button[] buttons;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        
+        private bool isEnd = false;
+        private bool isPreviousIsX = false;
+        
+        
+    }
+}
+
---
+++ 2term/kontr/X0/X0/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace X0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Initialize 9 button to XO.
+        private void InitializeButtons()
+        {
+            buttons = new System.Windows.Forms.Button[9];
+            for (int i = 0; i < 9; i++)
+            {
+                this.buttons[i] = new System.Windows.Forms.Button();
+                this.buttons[i].Location = new System.Drawing.Point(137, 72);
+                this.buttons[i].Name = "button" + i.ToString();
+                this.buttons[i].Size = new System.Drawing.Size(75, 23);
+                this.buttons[i].TabIndex = 0;
+                this.buttons[i].Text = null;
+                this.buttons[i].UseVisualStyleBackColor = true;
+                this.buttons[i].Dock = System.Windows.Forms.DockStyle.Fill;
+                int j = i;
+                this.buttons[i].Click += new System.EventHandler(
+                    (object sender, EventArgs e) => OnButtonClick(j));
+                this.tableLayoutPanel1.Controls.Add(this.buttons[i], i / 3, i % 3);
+            }
+        }
+
+        // If button is click.
+        private void OnButtonClick(int numberButton)
+        {
+            if (buttons[numberButton].Text != "" 
+                || (buttons[4].Text != "X" && buttons[4].Text != "O" && buttons[4].Text != ""))
+                return;
+            if (isPreviousIsX)
+                buttons[numberButton].Text = "O";
+            else
+                buttons[numberButton].Text = "X";
+            isPreviousIsX = !isPreviousIsX;
+
+            if (IsWin())
+                if (isPreviousIsX)
+                    buttons[4].Text = "X - win!!!";
+                else
+                    buttons[4].Text = "O - win!!!";
+            
+        }
+
+        // If someone win - return true.
+        private bool IsWin()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (buttons[3 * i].Text == buttons[3 * i + 1].Text
+                    && buttons[3 * i].Text == buttons[3 * i + 2].Text
+                    && buttons[3 * i].Text != "")
+                    return true;
+                if (buttons[i].Text == buttons[i + 3].Text
+                    && buttons[i].Text == buttons[i + 6].Text
+                    && buttons[i].Text != "")
+                    return true;
+            }
+            if (buttons[0].Text == buttons[4].Text
+                && buttons[0].Text == buttons[8].Text
+                && buttons[0].Text != "")
+                return true;
+            if (buttons[2].Text == buttons[4].Text
+                && buttons[2].Text == buttons[6].Text
+                && buttons[2].Text != "")
+                return true;
+            return false;
+        }
+    }
+}
