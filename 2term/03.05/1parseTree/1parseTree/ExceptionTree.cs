cea84c99de8071055cdc522b94691766136dba38
dyma96 <dyma96@bk.ru>
2014-03-31 00:33:14 +0400

    parseTree + ExceptionTree.cs

---
+++ 2term/03.05/1parseTree/1parseTree/ExceptionTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1parseTree
+{
+    /// <summary>
+    /// Class exception in tree.
+    /// </summary>
+    [Serializable]
+    public class ExceptionTree : ApplicationException
+    {
+        public ExceptionTree()
+        { }
+
+        public ExceptionTree(string message): base(message)
+        { }
+    }
+}
